e82c2fb84709b58f96127d1982fec3238409e985
Sergey Kudashev <kudashevs@gmail.com>
2024-11-23 23:30:09 +0300

    Update options schema add a position to some errors

--- test/unit/validator.test.js
+++ test/unit/validator.test.js
@@ -82,18 +82,80 @@ describe('options validator test suite', () => {
 
   it.each([
     [
+      'start in the first element is wrong',
+      {
+        blocks: [{start: 42, end: 'any', prefix: 'any', suffix: 'any'}],
+      },
+      /^blocks.0.start should be a string/,
+    ],
+    [
+      'end in the first element is wrong',
+      {
+        blocks: [{start: 'any', end: 42, prefix: 'any', suffix: 'any'}],
+      },
+      /^blocks.0.end should be a string/,
+    ],
+    [
+      'prefix in the first element is wrong',
+      {
+        blocks: [{start: 'any', end: 'any', prefix: 42, suffix: 'any'}],
+      },
+      /^blocks.0.prefix should be a string/,
+    ],
+    [
+      'suffix in the first element is wrong',
+      {
+        blocks: [{start: 'any', end: 'any', prefix: 'any', suffix: 42}],
+      },
+      /^blocks.0.suffix should be a string/,
+    ],
+    [
+      'prefix and suffix in the first element are wrong',
+      {
+        blocks: [{start: 'any', end: 'any', prefix: 42, suffix: 42}],
+      },
+      /^blocks.0.prefix should be a string and blocks.0.suffix should be a string/,
+    ],
+    [
+      'name in the second element is wrong',
+      {
+        blocks: [
+          {start: 'any', end: 'any', prefix: 'any', suffix: 'any'},
+          {start: 42, end: 'any', prefix: 'any', suffix: 'any'},
+        ],
+      },
+      /^blocks.1.start should be a string/,
+    ],
+  ])('fails when options.blocks type of the %s', (_, options, expected) => {
+    try {
+      sut(schema, options);
+    } catch (e) {
+      expect(e.message).toMatch(expected);
+    }
+    expect.assertions(1);
+  });
+
+  it.each([
+    [
       'first value is an object without start',
       {
-        blocks: [{prefix: 'any', suffix: 'any'}],
+        blocks: [{end: 'any', prefix: 'any', suffix: 'any'}],
       },
-      /^blocks.0 should be an object/,
+      /^blocks.0 should be a valid object/,
+    ],
+    [
+      'first element is an object with empty start',
+      {
+        blocks: [{start: '', end: 'any', prefix: 'any', suffix: 'any'}],
+      },
+      'start should be a non empty string',
     ],
     [
       'first value is an object without end',
       {
         blocks: [{start: 'any', prefix: 'any', suffix: 'any'}],
       },
-      /^blocks.0 should be an object/,
+      /^blocks.0 should be a valid object/,
     ],
     [
       'first value is an object with empty start',
@@ -112,7 +174,7 @@ describe('options validator test suite', () => {
     [
       'first value is an object with empty prefix',
       {
-        blocks: [{name: 'any', prefix: '', suffix: 'any'}],
+        blocks: [{start: 'any', end: 'any', prefix: '', suffix: 'any'}],
       },
       'prefix should be a non empty string',
     ],
@@ -124,9 +186,9 @@ describe('options validator test suite', () => {
           {prefix: 'any', suffix: 'any'},
         ],
       },
-      /^blocks.1 should be an object/,
+      /^blocks.1 should be a valid object/,
     ],
-  ])('fails when in options.blocks the %s', (_, options, expected) => {
+  ])('fails when options.blocks value of the %s', (_, options, expected) => {
     try {
       sut(schema, options);
     } catch (e) {
@@ -135,15 +197,15 @@ describe('options validator test suite', () => {
     expect.assertions(1);
   });
 
-  it('fails with a combined error when in options.blocks the first value is an object without suffix and empty start and prefix', () => {
+  it('fails with a combined error when in options.blocks the first value is an object without end and empty start and prefix', () => {
     const options = {
-      blocks: [{start: '', end: 'any', prefix: '', any: 'any'}],
+      blocks: [{start: '', prefix: '', any: 'any'}],
     };
 
     try {
       sut(schema, options);
     } catch (e) {
-      expect(e.message).toMatch(/^blocks.0 should be an object \(with start, end, prefix, suffix\) and prefix.+and.+start/);
+      expect(e.message).toMatch(/^blocks.0 should be a valid object with start, end, prefix, suffix and blocks.0.prefix.+and.+start/);
     }
     expect.assertions(1);
   });
@@ -157,16 +219,16 @@ describe('options validator test suite', () => {
     try {
       sut(schema, options, config);
     } catch (e) {
-      expect(e.message).toMatch(/start should be a non empty string and end.+and.+prefix/);
+      expect(e.message).toMatch(/blocks.0.start should be a non empty string and blocks.0.end.+and.+prefix/);
     }
     expect.assertions(1);
   });
 
-  it('fails with a combined error when in options.blocks the first and second values are objects without suffix and empty start and prefix', () => {
+  it('fails with a combined error when in options.blocks the first and second values are objects without end and empty start and prefix', () => {
     const options = {
       blocks: [
-        {start: '', end: 'any', prefix: '', any: 'any'},
-        {start: '', end: 'any', prefix: '', any: 'any'},
+        {start: '', prefix: '', any: 'any'},
+        {start: '', prefix: '', any: 'any'},
       ],
     };
     const config = {name: 'RemoveBlocks', orders: {blocks: ['start', 'end', 'prefix', 'suffix']}};
@@ -174,7 +236,7 @@ describe('options validator test suite', () => {
     try {
       sut(schema, options, config);
     } catch (e) {
-      expect(e.message).toMatch(/^blocks.0 should be an object \(with start, end, prefix, suffix\) and start.+and.+prefix.+and.+blocks.1/);
+      expect(e.message).toMatch(/^blocks.0 should be a valid object with start, end, prefix, suffix and blocks.0.start.+and.+prefix.+and.+blocks.1/);
     }
     expect.assertions(1);
   });
